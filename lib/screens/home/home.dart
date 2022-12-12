bb24dda4998b30b255305eecdc1e28349505e9e1
KibetAmon <kibetngetich805@gmail.com>
2022-12-12 14:20:57 +0300

    Added stylist model

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,4 @@
 import 'package:salon_booking_app/constants.dart';
 import 'package:salon_booking_app/Services/authentication.dart';
 import 'package:salon_booking_app/Services/database.dart';
+import 'package:salon_booking_app/models/stylist.dart';
